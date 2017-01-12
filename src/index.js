f5d98b5dd94b23bbd489ef97de3f6b92758d1dc9
unknown <1213833565@qq.com>
2017-01-12 19:46:49 +0800

    add react-router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,36 @@
 
 import React ,{Component} from 'react'
 import ReactDOM from 'react-dom'
-import {Router, Route, hashHistory} from 'react-router'
+import {Router, Route, hashHistory, Link, browserHistory} from 'react-router'
 import Buttons from './components/Buttons'
+import Project from './components/Project'
+import Contact from './components/Contact'
 
-ReactDOM.render(<Buttons />,document.getElementById("app"))
+const Home = React.createClass({
+    render() {
+        return(
+            <header className="header">
+                <div className="logo">jinxin</div>
+                <nav className="nav">
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/project">Project</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+                {this.props.children}
+            </header>
+        )
+    }
+}) 
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/"component={Home}>
+            <Route path="/project" component={Project}></Route>
+            <Route path="/contact" component={Contact}></Route>
+        </Route>
+    </Router>
+)
+    ,document.getElementById("app"))
 
 
